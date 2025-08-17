6898670ffe76b2d527fa146cacc02c1776f1099e
R-Tedy <stedyroland2@gmail.com>
2025-08-17 08:55:28 +0300

    first recursive function

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-
-
 const numberInput = document.getElementById("number-input");
 const convertBtn = document.getElementById("convert-btn");
 const result = document.getElementById("result");
 
 const decimalToBinary = (input) => {
-  let binary = "";
-
   if (input === 0) {
-    binary = "0";
+    return "0";
+  } else {
+    return decimalToBinary(Math.floor(input / 2)) + (input % 2);
   }
-
-  while (input > 0) {
-    binary = (input % 2) + binary;
-    input = Math.floor(input / 2);
-  }
-
-  result.innerText = binary;
 };
 
 const checkUserInput = () => {
@@ -29,7 +20,7 @@ const checkUserInput = () => {
     return;
   }
 
-  decimalToBinary(parseInt(numberInput.value));
+  result.textContent = decimalToBinary(parseInt(numberInput.value));
   numberInput.value = "";
 };
 
